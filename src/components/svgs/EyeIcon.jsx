cc088323ad6f04cde099792a619d2b9ab492ae3f
SMFaysalHaque <smfaysalhaque007@gmail.com>
2025-07-05 04:44:13 +0600

    style: add .svg  eye icon

---
+++ src/components/svgs/EyeIcon.jsx
@@ -0,0 +1,19 @@
+export default function EyeIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="w-4 h-4"
+    >
+      <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
+      <circle cx="12" cy="12" r="3" />
+    </svg>
+  );
+}
