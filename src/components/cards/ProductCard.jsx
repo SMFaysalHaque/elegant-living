51f9c85a0daf09101152c29cc41baebf842eaf90
SMFaysalHaque <smfaysalhaque007@gmail.com>
2025-07-05 15:49:22 +0600

    style: change card design as the task given.

--- src/components/cards/ProductCard.jsx
+++ src/components/cards/ProductCard.jsx
@@ -1,56 +1,56 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useCommaFormatter } from "../../hooks/useCommaFormatter";
 import EyeIcon from "../svgs/EyeIcon";
 
 export default function ProductCard({ product }) {
-  const [selectedImage, setSelectedImage] = useState(product.images[0]);
+  const { CommaFormatter } = useCommaFormatter();
 
   return (
-    <div className="bg-white rounded-2xl shadow-lg overflow-hidden transition-all duration-300 hover:shadow-xl hover:scale-105">
-      <div className="aspect-square bg-gray-50 overflow-hidden relative">
-        {/* main img */}
+    <div className="bg-white flex gap-3 rounded-2xl shadow-lg overflow-hidden transition-all duration-300 hover:shadow-xl hover:scale-105 w-full h-[250px] p-5">
+      <div className="relative w-full md:w-[200px] aspect-square bg-gray-100 rounded-lg overflow-hidden">
         <img
-          src={selectedImage}
+          src={
+            product.images[0].url ||
+            "https://via.placeholder.com/200?text=No+Image"
+          }
           alt="Modern Velvet Sofa"
           className="w-full h-full object-cover transition-all duration-300"
         />
         <div className="absolute top-3 left-3">
-          <span className="bg-green-500 text-white px-3 py-1 rounded-full text-sm font-semibold">
-            In Stock ({product.stock})
+          <span
+            className={`text-white px-2 py-1 rounded-full text-xs ${
+              product.stock > 2
+                ? "bg-green-500"
+                : product.stock > 0 && product.stock < 3
+                ? "bg-orange-500"
+                : product.stock == 0
+                ? "bg-red-500"
+                : ""
+            }`}
+          >
+            {product.stock > 2
+              ? `In Stock (${product.stock})`
+              : product.stock > 0 && product.stock < 3
+              ? `Only (${product.stock}) left`
+              : product.stock === 0
+              ? "Out of Stock"
+              : ""}
           </span>
         </div>
       </div>
 
-      <div className="p-3 bg-gray-50">
-        <div className="grid grid-cols-4 gap-2">
-          {product.images.map((img, idx) => (
-            <button
-              key={idx}
-              className={`aspect-square rounded-lg overflow-hidden transition-all hover:shadow-lg cursor-pointer ${
-                selectedImage === img ? "ring-2 ring-amber-500" : ""
-              }`}
-            >
-              <img
-                src={img}
-                alt={`thumb-${idx}`}
-                onClick={() => setSelectedImage(img)}
-                className="w-full h-full object-cover"
-              />
-            </button>
-          ))}
+      {/* Product Info */}
+      <div className="w-full flex flex-col justify-between">
+        <div className="flex flex-col gap-2">
+          <h3 className="text-xl font-bold text-gray-800">{product.title}</h3>
+          <p className="text-gray-600 line-clamp-1">{product.subtitle}</p>
+          <p className="text-2xl font-bold text-amber-600">
+            {CommaFormatter(product.price)} ৳
+          </p>
         </div>
-      </div>
 
-      <div className="p-6">
-        <h3 className="text-xl font-bold text-gray-800 mb-2">
-          {product.title}
-        </h3>
-        <p className="text-gray-600 mb-3">{product.subtitle}</p>
-        <p className="text-2xl font-bold text-amber-600 mb-4">
-          {product.price} ৳
-        </p>
         <Link to={`/product/${product.slug}`}>
-          <button className="inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium h-10 px-4 py-2 w-full bg-amber-600 hover:bg-amber-700 text-white transition-colors cursor-pointer">
+          <button className="inline-flex items-center justify-center gap-2 rounded text-sm font-medium h-10 px-4 py-2 w-fit bg-amber-600 hover:bg-amber-700 text-white transition-colors cursor-pointer">
             <EyeIcon />
             View Details
           </button>
