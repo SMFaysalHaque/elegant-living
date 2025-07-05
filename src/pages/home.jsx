0e1df3f29b7d8b5f8d40d66fd6a028711502658c
SMFaysalHaque <smfaysalhaque007@gmail.com>
2025-07-05 15:51:17 +0600

    style: update design layout

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
           transform your space into a sanctuary of style and comfort.
         </p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
