9b0d45e6d5d1b853f27f635347667ebdd4249fc8
SMFaysalHaque <smfaysalhaque007@gmail.com>
2025-07-06 00:03:56 +0600

    style: add icons.

---
+++ src/components/svgs/MessageIcon.jsx
@@ -0,0 +1,7 @@
+export default function MessageIcon() {
+  return (
+    <svg className="w-4 h-4 mr-2" fill="white" viewBox="0 0 24 24">
+      <path d="M20 4H4c-1 0-2 .8-2 2v12c0 1.1.9 2 2 2h16c1.2 0 2-.9 2-2V6c0-1.2-.8-2-2-2zm0 2v.5L12 13 4 6.5V6h16zM4 18V8l8 6 8-6v10H4z" />
+    </svg>
+  );
+}
---
+++ src/components/svgs/PhoneIcon.jsx
@@ -0,0 +1,7 @@
+export default function PhoneIcon() {
+  return (
+    <svg className="w-4 h-4 mr-2" fill="white" viewBox="0 0 24 24">
+      <path d="M3 5.8C3 15.5 8.5 21 18.2 21c1.1 0 2-.9 2-2v-2.2c0-.5-.4-1-.9-1l-3.3-.4c-.4 0-.8.2-1 .6l-1.3 2c-2.6-.7-4.8-2.9-5.5-5.5l2-1.3c.4-.2.6-.6.6-1L8.2 5.9c0-.5-.4-.9-1-.9H5c-.6 0-1 .5-1 1z" />
+    </svg>
+  );
+}
